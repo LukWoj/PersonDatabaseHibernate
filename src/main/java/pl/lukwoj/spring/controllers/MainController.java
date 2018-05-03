79c088dd51fc979f137ea0442809cd2f4adb2cca
Łukasz <lukasz.woje@wp.pl>
2018-05-03 02:49:52 +0200

    refactor

--- src/main/java/pl/lukwoj/spring/controllers/MainController.java
+++ src/main/java/pl/lukwoj/spring/controllers/MainController.java
@@ -9,33 +9,50 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.lukwoj.spring.model.SimpleBean;
 
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
 @Controller
 public class MainController {
+
 
     @Autowired
     SimpleBean simpleBean;
 
+    @RequestMapping("/lukasz")
+    @ResponseBody
+    public String main() {
+        return "lukasz";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseBody
+    public String response() {
+        return "Post method";
+    }
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String mainGet(Model model) {
-        model.addAttribute("someString", "String from Controller");
+    @ResponseBody
+    public String request() {
+        return "Get method: " + "random string from bean: " + simpleBean.generateString();
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String responseTemplate(Model model) {
+        LocalDateTime now = LocalDateTime.now();
+
+        if (now.getHour() > 1) {
+            model.addAttribute("someString", "Time to go home");
+        } else {
+            model.addAttribute("someString", "We are learning");
+        }
+
         return "index";
     }
 
     @RequestMapping(value = "/data", method = RequestMethod.POST)
-    @ResponseBody//ponieważ nie zwracamy nowego widoku
+    @ResponseBody
     public String postData(@RequestParam(value = "name", required = false) String nameFromForm) {
         return "Your name is: " + nameFromForm;
     }
 }
-
-    /*@RequestMapping(value = "/", method = RequestMethod.GET)
-    public String mainGet() {
-        return "index";
-    }*/
-
-    /*@RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
-    public String mainGet() {
-        return "<b>Hello Lukasz GET</b> : " + simpleBean.generateString();
-    }*/
-
